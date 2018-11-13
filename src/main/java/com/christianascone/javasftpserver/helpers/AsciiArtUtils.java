0023c93e70d3dea28a1e70679762eb162712961e
ChristianAscone <ascone.christian@gmail.com>
2018-11-13 09:14:46 +0100

    Add utils for Ascii art printing

---
+++ src/main/java/com/christianascone/javasftpserver/helpers/AsciiArtUtils.java
@@ -0,0 +1,18 @@
+package com.christianascone.javasftpserver.helpers;
+
+/**
+ * Simple utils class which provides a set of funny methods about ascii art printing
+ */
+public class AsciiArtUtils {
+    /**
+     * Print the project title using ascii art
+     */
+    public static void printProjectTitle() {
+        System.out.println("     ____.                       _________ _____  __             _________                                ");
+        System.out.println("    |    |____ ___  _______     /   _____// ____\\/  |_______    /   _____/ ______________  __ ___________ ");
+        System.out.println("    |    \\__  \\\\  \\/ /\\__  \\    \\_____  \\\\   __\\\\   __\\____ \\   \\_____  \\_/ __ \\_  __ \\  \\/ // __ \\_  __ \\");
+        System.out.println("/\\__|    |/ __ \\\\   /  / __ \\_  /        \\|  |   |  | |  |_> >  /        \\  ___/|  | \\/\\   /\\  ___/|  | \\/");
+        System.out.println("\\________(____  /\\_/  (____  / /_______  /|__|   |__| |   __/  /_______  /\\___  >__|    \\_/  \\___  >__|   ");
+        System.out.println("              \\/           \\/          \\/             |__|             \\/     \\/                 \\/       ");
+    }
+}
